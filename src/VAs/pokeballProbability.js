957c87888b83f3a4c9031df9efe13fa9d63be361
Franciele Faccin <francielefaccin@gmail.com>
2019-10-12 15:14:32 +0100

    fix pokeball probability

--- src/VAs/pokeballProbability.js
+++ src/VAs/pokeballProbability.js
@@ -4,9 +4,9 @@ export const getPokeball = () => {
   const x = Math.random();
   if (x <= 0.005)
     return POKEBALL_TYPE.MASTER;
-  if (x <= 0.095)
+  if (x > 0.005 && x <= 0.01)
     return POKEBALL_TYPE.ULTRA;
-  if (x <= .15)
+  if (x > 0.01 && x <= .25)
     return POKEBALL_TYPE.GREAT;
 
   return POKEBALL_TYPE.POKE;
