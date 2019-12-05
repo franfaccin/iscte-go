7e498ba886ff0d2e9efb18a71539fb59e8f862a3
vsfba1-iscteiul <vsfba1@iscte-iul.pt>
2019-12-05 22:44:31 +0000

    71 pokemons added | updated gitignore

--- src/config/pokemon-list.js
+++ src/config/pokemon-list.js
@@ -20,6 +20,414 @@ export const pokemonListComplete = [
     rarity: RARITY.RARE
   },
   {
+    name: "Charmander",
+    key: "charmander",
+    number: 4,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Charmeleon",
+    key: "charmeleon",
+    number: 5,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Charizard",
+    key: "charizard",
+    number: 6,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Squirtle",
+    key: "squirtle",
+    number: 7,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Wartortle",
+    key: "wartortle",
+    number: 8,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Blastoise",
+    key: "blastoise",
+    number: 9,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Caterpie",
+    key: "caterpie",
+    number: 10,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Metapod",
+    key: "metapod",
+    number: 11,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Butterfree",
+    key: "butterfree",
+    number: 12,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Weedle",
+    key: "weedle",
+    number: 13,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Kakuna",
+    key: "kakuna",
+    number: 14,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Beedrill",
+    key: "beedrill",
+    number: 15,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Pidgey",
+    key: "pidgey",
+    number: 16,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Pidgeotto",
+    key: "pidgeotto",
+    number: 17,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Pidgeot",
+    key: "pidgeot",
+    number: 18,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Rattata",
+    key: "rattata",
+    number: 19,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Raticate",
+    key: "raticate",
+    number: 20,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Spearow",
+    key: "spearow",
+    number: 21,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Fearow",
+    key: "fearow",
+    number: 22,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Ekans",
+    key: "ekans",
+    number: 23,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Arbok",
+    key: "arbok",
+    number: 24,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Pikachu",
+    key: "pikachu",
+    number: 25,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Raichu",
+    key: "raichu",
+    number: 26,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Sandshrew",
+    key: "sandshrew",
+    number: 27,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Sandslash",
+    key: "sandslash",
+    number: 28,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Nidoran ♀",
+    key: "nidoran-f",
+    number: 29,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Nidorina",
+    key: "nidorina",
+    number: 30,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Nidoqueen",
+    key: "nidoqueen",
+    number: 31,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Nidoran ♂",
+    key: "nidoran-m",
+    number: 32,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Nidorino",
+    key: "nidorino",
+    number: 33,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Nidoking",
+    key: "nidoking",
+    number: 34,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Clefairy",
+    key: "clefairy",
+    number: 35,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Clefable",
+    key: "clefable",
+    number: 36,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Vulpix",
+    key: "vulpix",
+    number: 37,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Ninetails",
+    key: "ninetails",
+    number: 38,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Jigglypuff",
+    key: "jigglypuff",
+    number: 39,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Wigglytuff",
+    key: "wigglytuff",
+    number: 40,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Zubat",
+    key: "zubat",
+    number: 41,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Golbat",
+    key: "golbat",
+    number: 42,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Oddish",
+    key: "oddish",
+    number: 43,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Gloom",
+    key: "gloom",
+    number: 44,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Vileplume",
+    key: "vileplume",
+    number: 45,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Paras",
+    key: "paras",
+    number: 46,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Parasect",
+    key: "parasect",
+    number: 47,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Venonat",
+    key: "venonat",
+    number: 48,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Venomoth",
+    key: "venomoth",
+    number: 49,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Diglett",
+    key: "diglett",
+    number: 50,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Dugtrio",
+    key: "dugtrio",
+    number: 51,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Meowth",
+    key: "meowth",
+    number: 52,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Persian",
+    key: "persian",
+    number: 53,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Psyduck",
+    key: "psyduck",
+    number: 54,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Golduck",
+    key: "golduck",
+    number: 55,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Mankey",
+    key: "mankey",
+    number: 56,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Primeape",
+    key: "primeape",
+    number: 57,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Growlithe",
+    key: "growlithe",
+    number: 58,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Arcanine",
+    key: "arcanine",
+    number: 59,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Poliwag",
+    key: "poliwag",
+    number: 60,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Poliwhirl",
+    key: "poliwhirl",
+    number: 61,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Poliwrath",
+    key: "poliwrath",
+    number: 62,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Abra",
+    key: "abra",
+    number: 63,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Kadabra",
+    key: "kadabra",
+    number: 64,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Alakazam",
+    key: "alakazam",
+    number: 65,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Machop",
+    key: "machop",
+    number: 66,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Machoke",
+    key: "machoke",
+    number: 67,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Machamp",
+    key: "machamp",
+    number: 68,
+    rarity: RARITY.RARE
+  },
+  {
+    name: "Bellsprout",
+    key: "bellsprout",
+    number: 69,
+    rarity: RARITY.COMMON
+  },
+  {
+    name: "Weepinbell",
+    key: "weepinbell",
+    number: 70,
+    rarity: RARITY.UNCOMMON
+  },
+  {
+    name: "Victreebel",
+    key: "victreebel",
+    number: 71,
+    rarity: RARITY.RARE
+  },
+  {
     name: "Mew",
     key: "mew",
     number: 151,
